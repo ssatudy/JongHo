7e87887fc5e2f3f5f480670f5278d8da45384437
jongho <ds.jongho1014@gmail.com>
2022-08-05 00:28:08 +0900

    solved

---
+++ day_2/7_two-sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for start in range(len(nums)):  
+            for end in range(start+1, len(nums)):  
+                if nums[start] + nums[end] == target:  
+                    return [start, end]
